0cef5d4565dc37fc9e3ef26f3ddc5b240a77240f
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2021-07-21 22:23:30 +0900

    Create GameManager.cs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public string userType;
+    public string nickName;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    void Start()
+    {
+        
+    }
+}
