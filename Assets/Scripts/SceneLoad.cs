dde4fe0325e5f5ca94f3fb29ce25b2b6ac1bc03a
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2021-07-21 22:24:00 +0900

    Update SceneLoad.cs

--- Assets/Scripts/SceneLoad.cs
+++ Assets/Scripts/SceneLoad.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 
 public class SceneLoad : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        LoadingSceneController.Instance.LoadScene("Scene2");
+        //LoadingSceneController.Instance.LoadScene("Scene2");
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void StartOption(string arg)
+    {
+        GameObject gm = GameObject.FindWithTag("GameManager");
+
+        if(arg == "1")
+        {
+            gm.GetComponent<GameManager>().userType = "1";
+        }
+        else if(arg == "2")
+        {
+            gm.GetComponent<GameManager>().userType = "2";
+        }
+        else
+        {
+            gm.GetComponent<GameManager>().userType = "3";
+        }
+
+        LoadingSceneController.Instance.LoadScene("Scene2");
     }
 }
