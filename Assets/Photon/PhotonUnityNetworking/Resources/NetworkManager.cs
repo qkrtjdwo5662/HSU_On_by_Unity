6ed8fb890e4490c6894a147419b5b1565d822a79
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-07-27 02:48:42 +0900

    서버 열기

---
+++ Assets/Photon/PhotonUnityNetworking/Resources/NetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+	string networkState;
+
+	void Start() => PhotonNetwork.ConnectUsingSettings();
+
+	public override void OnConnectedToMaster() =>
+		PhotonNetwork.JoinLobby();
+
+	public override void OnJoinedLobby() =>
+		PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions { MaxPlayers = 4 }, null);
+
+	void Update()
+	{
+		string curNetworkState = PhotonNetwork.NetworkClientState.ToString();
+		if (networkState != curNetworkState)
+		{
+			networkState = curNetworkState;
+			print(networkState);
+		}
+	}
+}
