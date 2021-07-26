cb984577def20b4aa50d846789dae46f4e8cd8c8
SU_VIN <67284987+SU-VIN@users.noreply.github.com>
2021-07-26 19:31:18 +0900

    정보창 생성

---
+++ Assets/Scripts/Start_Scene/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public GameObject player;
+
+    public float offsetX = 0f;
+    public float offsetY = 25f;
+    public float offsetZ = -35f;
+    Vector3 cameraPositon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        cameraPositon.x = player.transform.position.x + offsetX;
+        cameraPositon.y = player.transform.position.y + offsetY;
+        cameraPositon.z = player.transform.position.z + offsetZ;
+
+        transform.position = cameraPositon;
+    }
+}
