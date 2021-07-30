3d514fa6c95fd5c2c029abf346d70a8bcd0e02fc
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-07-31 01:15:24 +0900

    서버

--- Assets/Photon/PhotonUnityNetworking/Resources/NetworkManager.cs
+++ Assets/Photon/PhotonUnityNetworking/Resources/NetworkManager.cs
@@ -8,14 +8,20 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 {
 	string networkState;
 
-	void Start() => PhotonNetwork.ConnectUsingSettings();
-
-	public override void OnConnectedToMaster() =>
-		PhotonNetwork.JoinLobby();
-
-	public override void OnJoinedLobby() =>
+	private void Start()
+	{
+		Debug.Log("Connecting to Master");
+		PhotonNetwork.ConnectUsingSettings(); // 설정 포톤 서버에 따라 마스터 서버에 연결
+	}
+	public override void OnConnectedToMaster()
+	{
+		Debug.Log("Connecting to Master");
+		PhotonNetwork.JoinLobby(); // 마스터 서버 연결시 로비로 연결
+	}
+	public override void OnJoinedLobby()//로비 연결시 작동
+	{
 		PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions { MaxPlayers = 4 }, null);
-
+	}
 	void Update()
 	{
 		string curNetworkState = PhotonNetwork.NetworkClientState.ToString();
