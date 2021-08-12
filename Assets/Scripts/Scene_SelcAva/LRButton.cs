68afb2161356a3a9c0ef1d9af9d76f646c2c489a
yoongyu-hwang <dotheji11@naver.com>
2021-08-12 19:52:26 +0900

    아바타 선택창 회전

---
+++ Assets/Scripts/Scene_SelcAva/LRButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LRButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField]
+    private UI canvas;
+
+    [SerializeField]
+    private Transform character;
+
+
+
+    private bool isClick;
+    public enum BtnType { Left, Right, Next }
+    public BtnType btnType;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isClick = true;
+        
+        
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isClick = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isClick) {
+            switch (btnType)
+            {
+                case BtnType.Left:
+                    
+                    character.rotation = Quaternion.Euler(0, canvas.y+=2, 0);
+                    
+                    break;
+                case BtnType.Right:
+                    character.rotation = Quaternion.Euler(0, canvas.y-=2, 0);
+                    
+                    break;
+                case BtnType.Next:
+                    Debug.Log("NextButton is Click");
+                    break;
+            }
+        }
+    }
+}
