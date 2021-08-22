fa070ba5011b0630ec32dd6a5687647355e384c0
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-08-23 01:18:17 +0900

    채팅방

---
+++ Assets/Scripts/Scene_Field/ChatManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+using Photon.Pun;
+
+public class  ChatManager : MonoBehaviourPunCallbacks
+{
+    public List<string> chatList = new List<string>( );
+    public Button sendBtn;
+    public Text chatLog;
+    public Text chattingList;
+    public InputField input;
+    string chatters;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.IsMessageQueueRunning = true;
+        //scroll_rect = GameObject.FindObjectOfType<ScrollRect>();
+    }
+    public void SendButtonOnclicked()
+	{
+        if(input.text.Equals("")) { Debug.Log("Empty"); return; }
+        string msg = string.Format("[{0}] {1}", PhotonNetwork.NickName, input.text);
+        photonView.RPC("ReceiveMsg", RpcTarget.OthersBuffered, msg);
+        ReceiveMsg(msg);
+        input.ActivateInputField();
+        input.text = "";
+	}
+    void Update()
+	{
+        chatterUpdate();
+        if (Input.GetKeyDown(KeyCode.Return) && !input.isFocused) SendButtonOnclicked();
+	}
+    void chatterUpdate()
+    {
+        chatters = "Player List\n";
+        foreach(Player p in PhotonNetwork.PlayerList)
+		{
+            chatters += p.NickName + "\n";
+		}
+        chattingList.text = chatters;
+    }
+    [PunRPC]
+
+    public void ReceiveMsg(string msg)
+	{
+        chatLog.text += "\n" + msg;
+       // scroll_rect.verticalNormalizedPosition = 0.0f;
+	}
+}
