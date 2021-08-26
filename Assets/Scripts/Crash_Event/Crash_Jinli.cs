8fdaffa5e3a7e4e95b16a3158981744bd92b7cf6
woohozang <71766148+woohozang@users.noreply.github.com>
2021-08-27 00:14:07 +0900

    진리관 일부 건물충돌 이벤트
    진리관 외곽쪽 문 건물충돌 이벤트 구현, 도면 씬, 버튼 UI 삽입

---
+++ Assets/Scripts/Crash_Event/Crash_Jinli.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Crash_Jinli : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene("Scene_Jinli1");
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
+
