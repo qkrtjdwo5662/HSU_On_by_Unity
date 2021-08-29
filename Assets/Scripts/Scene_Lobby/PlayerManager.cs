7de0beb7b80a1c9ab49c18fcb26a3a7b969d1b11
yoongyu-hwang <dotheji11@naver.com>
2021-08-30 03:28:31 +0900

    .

--- Assets/Scripts/Scene_Lobby/PlayerManager.cs
+++ Assets/Scripts/Scene_Lobby/PlayerManager.cs
@@ -8,6 +8,7 @@ using System.IO;//path사용위해
 public class PlayerManager : MonoBehaviour
 {
     PhotonView PV;//포톤뷰 선언
+    [SerializeField]
     GameObject character;
 
     void Awake()
@@ -17,13 +18,17 @@ public class PlayerManager : MonoBehaviour
 
     void Start()
     {
-        if (PV.IsMine)//내 포톤 네트워크이면
+        /*if (PV.IsMine)//내 포톤 네트워크이면
         {
             CreateController();//플레이어 컨트롤러 붙여준다. 
         }
         else {
+            //GameObject card = (GameObject)Instantiate(Resources.Load("Prefabs/character"));
+            
+
             PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Character"), new Vector3(-108f, 0.1f, 80f), Quaternion.identity, 0, new object[] { PV.ViewID });
-        }
+        }*/
+        CreateController();
     }
     void CreateController()//플레이어 컨트롤러 만들기
     {
@@ -31,4 +36,11 @@ public class PlayerManager : MonoBehaviour
         Debug.Log("Instantiated Controller");
         PhotonNetwork.Instantiate(Path.Combine("Prefabs", "Character"), new Vector3(-108f,0.1f,80f), Quaternion.identity,0,new object[] { PV.ViewID });
     }
+
+    void Update()
+    {
+        
+    }
+
+
 }
