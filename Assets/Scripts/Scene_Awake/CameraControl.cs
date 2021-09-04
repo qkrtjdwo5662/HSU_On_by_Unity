1864ed05ecf51a2a187d35e5f8f11f1c5ee3c085
yoongyu-hwang <dotheji11@naver.com>
2021-09-05 02:31:56 +0900

    Scene_Awake 추가

---
+++ Assets/Scripts/Scene_Awake/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    
+    public GameObject cam1;
+    public GameObject cam2;
+    public GameObject cam3;
+    public GameObject cam4;
+    public GameObject cam5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cam1.transform.position += new Vector3(0.01f, 0, 0);
+    }
+}
---
+++ Assets/Scripts/Scene_Awake/Next.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Next : MonoBehaviour
+{
+    public float LimitTime;
+    
+
+    public Button NextButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextButton.onClick.AddListener(ButtonClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ButtonClick() {
+        /*for (int i = 100; i <= 255; i++) {
+            NextButton.image.color += new Color(0,0,0,i);
+        }*/
+
+        LoadingSceneController.Instance.LoadScene("Scene1");
+    }
+}
