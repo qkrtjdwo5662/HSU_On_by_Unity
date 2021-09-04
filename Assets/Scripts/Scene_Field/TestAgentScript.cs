80a7f7ee6650429bff3e8ca1fe18a32e1ad02e18
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-09-05 01:39:15 +0900

    ai구현1

---
+++ Assets/Scripts/Scene_Field/TestAgentScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Navigation 관련 기능을 사용할 때 필요.
+using UnityEngine.AI;
+
+public class TestAgentScript : MonoBehaviour
+{
+    // 목표 지점
+    public List<Transform> wayPoints;
+
+    public int nextIdx=0;
+
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.autoBraking = false;
+
+        var group = GameObject.Find("WayPointGroup");
+
+        if (group != null)
+		{
+            group.GetComponentsInChildren<Transform>(wayPoints);
+            wayPoints.RemoveAt(0);
+		}
+
+        MoveWayPoint();
+    }
+    
+    private void MoveWayPoint()
+	{
+        if (agent.isPathStale) return;
+
+        agent.destination = wayPoints[nextIdx].position;
+        agent.isStopped = false;
+	}
+
+    void Update()
+    {
+       if(agent.velocity.sqrMagnitude >=0.2f * 0.2f
+            && agent.remainingDistance <= 0.5f)
+		{
+            nextIdx = ++nextIdx % wayPoints.Count;
+            MoveWayPoint();
+		}            
+    }
+}
