bee4738d6f21b390c186ac0c36319e998a9f805b
woohozang <71766148+woohozang@users.noreply.github.com>
2021-09-11 23:39:41 +0900

    Canvas 건물 정보 검색 기능 추가
    Information 폴더 생성 - 건물 정보 이미지, 기존 있었던 씬 제거(실행 지장X), 씬 불러오는 스크립트 제거, 캔버스 UI에 검색 기능 버튼 추가

---
+++ Assets/Scripts/Crash_Event/Crash_Object.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Crash_Object : MonoBehaviour
+{
+    private Button btn;
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+
+    }
+}
+
