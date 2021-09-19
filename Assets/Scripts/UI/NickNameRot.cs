f6d37b19dbb4fb04bdf3a02108adcd36cf6b4cc8
yoongyu-hwang <dotheji11@naver.com>
2021-09-19 15:07:54 +0900

    닉네임바 구현

---
+++ Assets/Scripts/UI/NickNameRot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NickNameRot : MonoBehaviour
+{
+    GameObject CameraArm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CameraArm = GameObject.Find("Camera Arm");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = CameraArm.transform.rotation;
+    }
+}
