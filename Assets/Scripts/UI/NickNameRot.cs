6306205a8fd9f7f85204df661305740e3e83eeda
yoongyu-hwang <dotheji11@naver.com>
2021-09-20 17:30:47 +0900

    닉네임 완성

--- Assets/Scripts/UI/NickNameRot.cs
+++ Assets/Scripts/UI/NickNameRot.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Photon.Pun;
+using Photon.Realtime;
 public class NickNameRot : MonoBehaviour
 {
     GameObject CameraArm;
     Text NickName;
     public GameObject TMP;
+    PhotonView PV;
     // Start is called before the first frame update
     void Start()
     {
+        PV = GetComponent<PhotonView>();
         CameraArm = GameObject.Find("Camera Arm");
-        NickName = GameObject.Find("NickName").GetComponent<Text>();
-        TMP.GetComponent<TextMeshPro>().text = NickName.text;
+        if (PV.IsMine)
+        {
+            NickName = GameObject.Find("NickName").GetComponent<Text>();
+            PV.RPC("set", RpcTarget.OthersBuffered, NickName.text);
+            set(NickName.text);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.rotation = CameraArm.transform.rotation;
+    }
+    [PunRPC]
+    private void set(string msg)
+    {
+        TMP.GetComponent<TextMeshPro>().text = msg;
     }
 }
