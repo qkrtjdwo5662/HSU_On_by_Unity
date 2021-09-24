11d8a59e9fb5e0de7cb1e32cf350ded1da473e64
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-09-24 21:59:39 +0900

    퀘스트 구현(생각중.....)

---
+++ Assets/Scripts/Scene_Field/Quest/QuestManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public int questId;
+
+    Dictionary<int, QuestData> questList;
+
+    void Awake()
+    {
+        questList = new Dictionary<int, QuestData>();
+        GenerateData();
+    }
+ 
+    void GenerateData()
+	{
+        //questList.Add(10, new QuestData("첫 번째 퀘스트", new int[1] { }));
+	}
+    
+    public int GetQuestTalkIndex(int id)
+	{
+        return questId;
+	}
+    // Start is called before the first frame update
+}
---
+++ Assets/Scripts/Scene_Field/Quest/TalkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkManager : MonoBehaviour
+{
+    Dictionary<int, string[]> talkData;//대화 데이터 저장
+
+    void Awake()
+    {
+        talkData = new Dictionary<int, string[]>();
+        GenerateData();
+    }
+
+    // Update is called once per frame
+    void GenerateData()
+    {
+       talkData.Add(100, new string[] { "안녕! 난 100번 npc야" });
+
+    }
+    
+    /*public string GetTalk(int id, int talkIndex)//대사를 전달
+	{
+        return TalkData[id][talkIndex];
+	}
+    */
+}  
