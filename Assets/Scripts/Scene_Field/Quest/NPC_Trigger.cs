d8c6f69e2cf2f3ed787fbe9b37f5549b40f8489b
woohozang <71766148+woohozang@users.noreply.github.com>
2021-09-26 03:56:19 +0900

    NPC 대화 일부 구현
    씬 필드 캔버스에 npc 대화 gameobject 추가 ui 추가 npc 충돌 시 대화, 대화 이어가기 버튼 추가

---
+++ Assets/Scripts/Scene_Field/Quest/NPC_Trigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPC_Trigger : MonoBehaviour
+{
+    public GameObject NpcTalk;
+    // Start is called before the first frame update
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            NpcTalk.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        NpcTalk.SetActive(false);
+    }
+}
+
