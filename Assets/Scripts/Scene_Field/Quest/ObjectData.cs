772178e18acde936fbf05179bb3049bee8858582
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-09-25 12:27:24 +0900

    npc 터치 이벤트 구현중..

--- Assets/Scripts/Scene_Field/Quest/ObjectData.cs
+++ Assets/Scripts/Scene_Field/Quest/ObjectData.cs
@@ -1,10 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectData : MonoBehaviour
 {
     // Start is called before the first frame update
     public int id;
     public bool isNpc;
+    public Image image;
+    public Text text;
+	public Camera MainCamera;
+	
+	/*void Start()
+	{
+
+		MainCamera = Camera.main;
+	}
+
+    void Update()
+    {
+		Ray ray = MainCamera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+
+		if (Physics.Raycast(ray, out hit))
+		{
+			Debug.Log(hit.transform.gameObject);
+
+			/*if (hit.transform.gameObject.id = "100")
+			{
+				
+
+			}
+		}
+	}
+	*/
+   
+       
 }
