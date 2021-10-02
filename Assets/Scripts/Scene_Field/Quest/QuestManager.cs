d8e68c38e86a51d6433735bd45bd4c904b9c0ddc
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-10-03 04:50:40 +0900

    미션 성공시 쾌스트창 변화 작업

--- Assets/Scripts/Scene_Field/Quest/QuestManager.cs
+++ Assets/Scripts/Scene_Field/Quest/QuestManager.cs
@@ -1,10 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestManager : MonoBehaviour
 {
     public int questId;
+    public InputField question1;
+    public Button H1;
+    public Button H2;
+    public Button H3;
+    public Button H4;
+    public Button H5;
+    public Button Submit;
+    public Image right;
+    public Image Wrong;
+    public Image Stamp1;
 
     Dictionary<int, QuestData> questList;
 
@@ -13,7 +24,38 @@ public class QuestManager : MonoBehaviour
         questList = new Dictionary<int, QuestData>();
         GenerateData();
     }
- 
+
+    void Start()
+    {
+        H1.interactable = false;
+        H2.interactable = false;
+        H3.interactable = false;
+        H4.interactable = false;
+        H5.interactable = false;
+    }
+    public void Button()
+    {
+
+            if (question1.text == "2")
+            {
+                right.gameObject.SetActive(true);
+            }
+            else
+            {
+                Wrong.gameObject.SetActive(true);
+            }
+      
+    }
+
+    public void Complete()
+	{
+        Stamp1.gameObject.SetActive(true);
+	}
+
+  
+
+   
+
     void GenerateData()
 	{
         //questList.Add(10, new QuestData("첫 번째 퀘스트", new int[1] { }));
