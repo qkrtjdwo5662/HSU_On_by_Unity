a70a3ffb3b80f113d7134bf5394c94e945f8db1f
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-10-03 17:33:42 +0900

    쾌스트 이래저래

--- Assets/Scripts/Scene_Field/Quest/NPC_Trigger.cs
+++ Assets/Scripts/Scene_Field/Quest/NPC_Trigger.cs
@@ -6,7 +6,9 @@ using UnityEngine.UI;
 public class NPC_Trigger : MonoBehaviour
 {
     public GameObject NpcTalk;
+    public Image Talk1;
     public GameObject NpcTalkSub;
+    public Button YES_Button;
     // Start is called before the first frame update
 
     void OnTriggerEnter(Collider other)
@@ -15,7 +17,13 @@ public class NPC_Trigger : MonoBehaviour
         {
             NpcTalk.SetActive(true);
             NpcTalkSub.SetActive(false);
+            Talk1.gameObject.SetActive(true);
         }
+    }
+
+    public void YesButtonClick()
+    {
+        Talk1.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
