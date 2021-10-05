90abad2e9e7e51afab99dc890d6106bfbb8a118c
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-10-06 04:42:33 +0900

    쾌스트 오브젝트 수정

--- Assets/Scripts/Scene_Field/Quest/NPC_Trigger.cs
+++ Assets/Scripts/Scene_Field/Quest/NPC_Trigger.cs
@@ -5,39 +5,31 @@ using UnityEngine.UI;
 
 public class NPC_Trigger : MonoBehaviour
 {
-    public GameObject NpcTalk;
-    public GameObject YesAfter;
-    public GameObject NpcTalkSub;
+    public GameObject Dialog;
+    public GameObject MainNpcTalk;
+  
     
-    public Button YES_Button;
-    //public Image Talk0;
-    // Start is called before the first frame update
+   
 
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            NpcTalk.SetActive(true);
-            NpcTalkSub.SetActive(false);
-           
-            YesAfter.SetActive(true);
+            Dialog.SetActive(true);
+
+            MainNpcTalk.SetActive(true);
+            
+          
             
         }
     }
 
-    public void YesButtonClick()
-    {
-        
-           //Talk0.gameObject.SetActive(true);
-        
-    }
-
+   
     // Update is called once per frame
     void OnTriggerExit(Collider other)
     {
-        NpcTalk.SetActive(false);
-        NpcTalkSub.SetActive(false);
-        YesAfter.SetActive(false);
+        Dialog.SetActive(false);
+       
     }
 }
 
