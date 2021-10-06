ff02d1c325ebc08aef686d2c81119cf3f4ceea0a
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2021-10-06 11:18:26 +0900

    쾌스트 직소 퍼즐 기능 초안

---
+++ Assets/Scripts/Scene_Field/Puzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Puzzle : MonoBehaviour
+{
+    public GameObject puzzlePieceSet;
+    public GameObject puzzlePosSet;
+    // Start is called before the first frame update
+    public bool IsClear()
+    {
+        for (int i = 0; i < puzzlePosSet.transform.childCount; i++)
+        {
+            //퍼즐위치의 자식이 없으면 모든 퍼즐조각이 놓여지지 않은것입니다.
+            if (puzzlePosSet.transform.GetChild(i).childCount == 0)
+            {
+                return false;
+            }
+            //퍼즐조각의 번호와 퍼즐 위치 번호가 일치하지 않으면 퍼즐은 완성되지 않은것입니다.
+            if (puzzlePosSet.transform.GetChild(i).GetChild(0).GetComponent<PuzzlePiece>().piece_no != i)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
