6107cba67e1543b617d029aea09c8e9bc2c22077
woohozang <71766148+woohozang@users.noreply.github.com>
2021-10-08 02:40:10 +0900

    보물찾기 퀘스트 추가
    npc5번 추가, 보물 오브젝트 3개 찾는 방식 게임 추가, 보물 트리거 스크립트 추가

---
+++ Assets/Scripts/Scene_Field/Quest/Bomul_Trigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bomul_Trigger : MonoBehaviour
+{
+    public GameObject Bomul;
+    public GameObject BomulSub;
+  
+    
+   
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Bomul.SetActive(true);
+            
+            
+            
+          
+            
+        }
+    }
+
+   
+    // Update is called once per frame
+    void OnTriggerExit(Collider other)
+    {
+        Bomul.SetActive(false);
+        
+    }
+}
+
