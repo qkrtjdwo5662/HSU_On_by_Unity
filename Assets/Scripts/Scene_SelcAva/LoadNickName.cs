8dcdc29fcf2a6c816c2b7c1018e56d9ff541dd72
yoongyu-hwang <dotheji11@naver.com>
2021-10-12 18:35:13 +0900

    오류 수정

---
+++ Assets/Scripts/Scene_SelcAva/LoadNickName.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadNickName : MonoBehaviour
+{
+    Text NickName;
+    public GameObject TMP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        NickName = GameObject.Find("NickName").GetComponent<Text>();
+        TMP.GetComponent<TextMeshPro>().text = NickName.text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
