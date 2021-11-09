9749b829482989420ce9550a22e6995b5062b7ea
yoongyu-hwang <dotheji11@naver.com>
2021-11-09 19:41:46 +0900

    볼륨조절, 게임종료기능 추가

---
+++ Assets/Scripts/Scene_Field/QuitAction.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class QuitAction : MonoBehaviour
+{
+    public Button button;
+    public Slider slider;
+    public AudioSource AS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(ExitGame);
+        AS = GameObject.Find("Main Camera").GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float a = slider.value;
+        AS.volume = a * 0.125f;
+    }
+
+    void ExitGame() {
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+
+    void volume() { 
+    
+    }
+
+}
