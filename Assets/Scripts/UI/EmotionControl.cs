3230ae8eb768596296133c6b67c1c54931b267da
yoongyu-hwang <dotheji11@naver.com>
2022-01-06 11:43:58 +0900

    스크립트 간소화, 만든사람들 이미지 배치 수정

--- Assets/Scripts/UI/EmotionControl.cs
+++ Assets/Scripts/UI/EmotionControl.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class EmotionControl : MonoBehaviour
@@ -33,77 +31,77 @@ public class EmotionControl : MonoBehaviour
             victory.onClick.AddListener(DoVictoy);
             lose.onClick.AddListener(DoLose);
         }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
-
-        void DoConversation()
-        {
-            animator.SetBool("conversation", true);
-            animator.SetBool("dance", false);
-            animator.SetBool("victory", false);
-            animator.SetBool("lose", false);
-            animator.SetBool("yes", false);
-            animator.SetBool("no", false);
-            EmotionPanel.SetActive(false);
-        }
-
-        void DoDance()
-        {
-            animator.SetBool("dance", true);
-            animator.SetBool("conversation", false);
-            animator.SetBool("victory", false);
-            animator.SetBool("lose", false);
-            animator.SetBool("yes", false);
-            animator.SetBool("no", false);
-            EmotionPanel.SetActive(false);
-        }
-
-        void DoYes()
-        {
-            animator.SetBool("yes", true);
-            animator.SetBool("conversation", false);
-            animator.SetBool("dance", false);
-            animator.SetBool("victory", false);
-            animator.SetBool("lose", false);
-            animator.SetBool("no", false);
-            EmotionPanel.SetActive(false);
-        }
-        void DoNo()
-        {
-            animator.SetBool("no", true);
-            animator.SetBool("conversation", false);
-            animator.SetBool("dance", false);
-            animator.SetBool("victory", false);
-            animator.SetBool("lose", false);
-            animator.SetBool("yes", false);
-            EmotionPanel.SetActive(false);
-        }
-
-        void DoVictoy()
-        {
-            animator.SetBool("victory", true);
-            animator.SetBool("conversation", false);
-            animator.SetBool("dance", false);
-            animator.SetBool("lose", false);
-            animator.SetBool("yes", false);
-            animator.SetBool("no", false);
-            EmotionPanel.SetActive(false);
-        }
-
-        void DoLose()
-        {
-            animator.SetBool("lose", true);
-            animator.SetBool("conversation", false);
-            animator.SetBool("dance", false);
-            animator.SetBool("victory", false);
-            animator.SetBool("yes", false);
-            animator.SetBool("no", false);
-            EmotionPanel.SetActive(false);
-
-        }
     }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void DoConversation()
+    {
+        animator.SetBool("conversation", true);
+        animator.SetBool("dance", false);
+        animator.SetBool("victory", false);
+        animator.SetBool("lose", false);
+        animator.SetBool("yes", false);
+        animator.SetBool("no", false);
+        EmotionPanel.SetActive(false);
+    }
+
+    void DoDance()
+    {
+        animator.SetBool("dance", true);
+        animator.SetBool("conversation", false);
+        animator.SetBool("victory", false);
+        animator.SetBool("lose", false);
+        animator.SetBool("yes", false);
+        animator.SetBool("no", false);
+        EmotionPanel.SetActive(false);
+    }
+
+    void DoYes()
+    {
+        animator.SetBool("yes", true);
+        animator.SetBool("conversation", false);
+        animator.SetBool("dance", false);
+        animator.SetBool("victory", false);
+        animator.SetBool("lose", false);
+        animator.SetBool("no", false);
+        EmotionPanel.SetActive(false);
+    }
+    void DoNo()
+    {
+        animator.SetBool("no", true);
+        animator.SetBool("conversation", false);
+        animator.SetBool("dance", false);
+        animator.SetBool("victory", false);
+        animator.SetBool("lose", false);
+        animator.SetBool("yes", false);
+        EmotionPanel.SetActive(false);
+    }
+
+    void DoVictoy()
+    {
+        animator.SetBool("victory", true);
+        animator.SetBool("conversation", false);
+        animator.SetBool("dance", false);
+        animator.SetBool("lose", false);
+        animator.SetBool("yes", false);
+        animator.SetBool("no", false);
+        EmotionPanel.SetActive(false);
+    }
+
+    void DoLose()
+    {
+        animator.SetBool("lose", true);
+        animator.SetBool("conversation", false);
+        animator.SetBool("dance", false);
+        animator.SetBool("victory", false);
+        animator.SetBool("yes", false);
+        animator.SetBool("no", false);
+        EmotionPanel.SetActive(false);
+
+    }
+
 }
