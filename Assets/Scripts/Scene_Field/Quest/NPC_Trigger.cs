87404d9d55d2e83d0ab6414e0670571aa5e2ccc5
yoongyu-hwang <dotheji11@naver.com>
2022-02-09 10:54:52 +0900

    카메라워크 중단 (추후논의)

--- Assets/Scripts/Scene_Field/Quest/NPC_Trigger.cs
+++ Assets/Scripts/Scene_Field/Quest/NPC_Trigger.cs
@@ -9,8 +9,20 @@ public class NPC_Trigger : MonoBehaviour
     public GameObject Dialog;
     public GameObject MainNpcTalk;
     public GameObject TalkStart;
+
+   /* private Button btn; //대화하기 버튼
+    private GameObject CameraArm;
+    private GameObject Me;*/
     
-   
+
+
+/*    private void Start()
+    {
+        Me = GameObject.Find("Me");
+        CameraArm = Me.transform.Find("Camera Arm").gameObject;
+        btn = MainNpcTalk.transform.Find("Talk0").GetComponentInChildren<Button>();
+        btn.onClick.AddListener(CameraWork);
+    }*/
 
     void OnTriggerEnter(Collider other)
     {
@@ -35,6 +47,30 @@ public class NPC_Trigger : MonoBehaviour
         MainNpcTalk.SetActive(false);
         if (!TalkStart.Equals(null))
             TalkStart.SetActive(false);
+        //StartCoroutine(MoveTo(CameraArm, Me.transform.position));
     }
+
+/*    private void CameraWork() {
+        StartCoroutine(MoveTo(CameraArm, this.transform.position + new Vector3(1f,0.9f,-2.4f)));
+    }
+
+    IEnumerator MoveTo(GameObject obj, Vector3 destination) 
+    {
+        float count = 0;
+        Vector3 wasPos = obj.transform.position;
+
+        while (true) 
+        {
+            count += Time.deltaTime;
+            obj.transform.position = Vector3.Lerp(wasPos, destination, count);
+
+            if (count >= 1)
+            {
+                obj.transform.position = destination;
+                break;
+            }
+            yield return null;
+        }
+    }*/
 }
 
