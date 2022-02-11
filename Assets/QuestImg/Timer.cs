ee1ace052a634dac2966467964587cf0d66e8476
woohozang <71766148+woohozang@users.noreply.github.com>
2022-02-12 00:47:16 +0900

    랜덤뽑기 틀 제작
    .

---
+++ Assets/QuestImg/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject Test1;
+    public GameObject Test3;
+    public float LimitTime = 60;
+    public Text text_Timer;
+    // Start is called before the first frame update
+
+    public void DoTimerOffset()
+    {
+        LimitTime = 60;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        LimitTime -= Time.deltaTime;
+        text_Timer.text = "남은 시간 :   " + Mathf.Round(LimitTime);
+        if (LimitTime <= 0)
+        {
+            Test1.SetActive(false);
+            Test3.SetActive(true);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Scene_Field/Quest/RandomBox.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomBox : MonoBehaviour
+{
+    public GameObject DrawShop;
+    public GameObject DrawWindow;
+    public GameObject S;
+    public GameObject A;
+    public GameObject B;
+    public Image DrawImage;
+    public Sprite Image1;
+    public Sprite Image2;
+    public Sprite Image3;
+    public Sprite Image4;
+    public Sprite Image5;
+    public Sprite Image6;
+    public Sprite Image7;
+    public Sprite Image8;
+    public Sprite Image9;
+    public Sprite Image10;
+
+    public int RandomInt;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RandomInt = Random.Range(0, 100);
+    }
+    public void Draw()
+    {
+        DrawShop.SetActive(false);
+        DrawWindow.SetActive(true);
+        if (RandomInt <= 10)
+        {
+            DrawImage.sprite = Image1; //슈퍼레어
+            S.SetActive(true);
+        }
+        else if (RandomInt > 10 && RandomInt <= 50)
+        {
+            DrawImage.sprite = Image2; //레어
+            A.SetActive(true);
+        }
+        else if (RandomInt > 50 && RandomInt <= 100)
+        {
+            DrawImage.sprite = Image3; //노멀
+            B.SetActive(true);
+        }
+    }
+
+    public void ReButton()
+    {
+        DrawShop.SetActive(true);
+        DrawWindow.SetActive(false);
+        S.SetActive(false);
+        A.SetActive(false);
+        B.SetActive(false);
+    }
+}
