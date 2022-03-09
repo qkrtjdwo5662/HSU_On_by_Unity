2cd2843837246851b2407a34dfded6cdaac9d31d
qkrtjdwo5662 <qkrtjdwo5662@naver.com>
2022-03-10 06:05:28 +0900

    migration 준비완료
    json규칙 문제 였음;

--- Assets/Scripts/Scene_Awake/Migration.cs
+++ Assets/Scripts/Scene_Awake/Migration.cs
@@ -9,79 +9,89 @@ using UnityEngine.UI;
 
 public class Migration : MonoBehaviour
 {
-    public Text text;
-    public Text verText;
-    
-    public DatabaseReference reference = null;
+	public Text text;
+	public Text verText;
+
+	public DatabaseReference reference;
 
 
-    public DataSnapshot ds;
+	public DataSnapshot ds;
+
+	// Start is called before the first frame update
+	public List<string> UID = new List<string>()
+	{
+		//Userid
+	};
 
 
-    // Start is called before the first frame update
+	void Start()
+	{
+		FirebaseApp.DefaultInstance.Options.DatabaseUrl =
+				   new System.Uri("https://hsuon-4c8e4-default-rtdb.firebaseio.com/");
 
-    
-
-    void Start()
-    {
-        FirebaseApp.DefaultInstance.Options.DatabaseUrl =
-                   new System.Uri("https://hsuon-4c8e4-default-rtdb.firebaseio.com/");
-        reference = FirebaseDatabase.DefaultInstance.GetReference("user");
+		reference = FirebaseDatabase.DefaultInstance.RootReference;
 
 
-/*        for (int i = 0; i < stringArray.length; i++)
-        {
-            MigrationFireBase(stringArray[i]);
-            Debug.Log(i + " cleared");
-        }*/
-    }
+		for (int i = 0; i < UID.Count; i++)
+		{
+			MigrationFireBase(UID[i]);
+			Debug.Log(i + " cleared");
+		}
+	}
 
 
-    void MigrationFireBase(string UID)
-    {
-        reference = reference.Child(UID);
-        reference.Child("HC01").SetValueAsync(false);
-        reference.Child("HC02").SetValueAsync(false);
-        reference.Child("HC03").SetValueAsync(false);
-        reference.Child("HC04").SetValueAsync(false);
-        reference.Child("HC05").SetValueAsync(false);
-        reference.Child("HC06").SetValueAsync(false);
-        reference.Child("HC07").SetValueAsync(false);
+	void MigrationFireBase(string UID)
+	{
+		reference = FirebaseDatabase.DefaultInstance.GetReference("users").Child(UID);
 
-        reference.Child("C01").SetValueAsync(false);
-        reference.Child("C02").SetValueAsync(false);
-        reference.Child("C03").SetValueAsync(false);
-        reference.Child("C04").SetValueAsync(false);
-        reference.Child("C05").SetValueAsync(false);
-        reference.Child("C06").SetValueAsync(false);
-        reference.Child("C07").SetValueAsync(false);
-        reference.Child("C08").SetValueAsync(false);
-        reference.Child("C09").SetValueAsync(false);
-        reference.Child("C10").SetValueAsync(false);
+		//필드추가 : reference.Child("필드이름").SetValueAsync();
+		//필드삭제 : reference.Child("필드이름").RemoveValueAsync();
 
-        reference.Child("B01").SetValueAsync(false);
-        reference.Child("B02").SetValueAsync(false);
-        reference.Child("B03").SetValueAsync(false);
-        reference.Child("B04").SetValueAsync(false);
-        reference.Child("B05").SetValueAsync(false);
-        reference.Child("B06").SetValueAsync(false);
-        reference.Child("B07").SetValueAsync(false);
-        reference.Child("B08").SetValueAsync(false);
-        reference.Child("B09").SetValueAsync(false);
-        reference.Child("B10").SetValueAsync(false);
+		reference.Child("HC01").SetValueAsync(false);
+		reference.Child("HC02").SetValueAsync(false);
+		reference.Child("HC03").SetValueAsync(false);
+		reference.Child("HC04").SetValueAsync(false);
+		reference.Child("HC05").SetValueAsync(false);
+		reference.Child("HC06").SetValueAsync(false);
+		reference.Child("HC07").SetValueAsync(false);
+		reference.Child("HC08").SetValueAsync(false);
+		reference.Child("HC09").SetValueAsync(false);
+		reference.Child("HC10").SetValueAsync(false);
 
-        reference.Child("P01").SetValueAsync(false);
-        reference.Child("P02").SetValueAsync(false);
-        reference.Child("P03").SetValueAsync(false);
-        reference.Child("P04").SetValueAsync(false);
-        reference.Child("P05").SetValueAsync(false);
-        reference.Child("P06").SetValueAsync(false);
-        reference.Child("P07").SetValueAsync(false);
-        reference.Child("P08").SetValueAsync(false);
-        reference.Child("P09").SetValueAsync(false);
-        reference.Child("P10").SetValueAsync(false);
+		reference.Child("C01").SetValueAsync(false);
+		reference.Child("C02").SetValueAsync(false);
+		reference.Child("C03").SetValueAsync(false);
+		reference.Child("C04").SetValueAsync(false);
+		reference.Child("C05").SetValueAsync(false);
+		reference.Child("C06").SetValueAsync(false);
+		reference.Child("C07").SetValueAsync(false);
+		reference.Child("C08").SetValueAsync(false);
+		reference.Child("C09").SetValueAsync(false);
+		reference.Child("C10").SetValueAsync(false);
 
-        
-    }
+		reference.Child("PET01").SetValueAsync(false);
+		reference.Child("PET02").SetValueAsync(false);
+		reference.Child("PET03").SetValueAsync(false);
+		reference.Child("PET04").SetValueAsync(false);
+		reference.Child("PET05").SetValueAsync(false);
+		reference.Child("PET06").SetValueAsync(false);
+		reference.Child("PET07").SetValueAsync(false);
+		reference.Child("PET08").SetValueAsync(false);
+		reference.Child("PET09").SetValueAsync(false);
+		reference.Child("PET10").SetValueAsync(false);
 
+		reference.Child("P01").SetValueAsync(false);
+		reference.Child("P02").SetValueAsync(false);
+		reference.Child("P03").SetValueAsync(false);
+		reference.Child("P04").SetValueAsync(false);
+		reference.Child("P05").SetValueAsync(false);
+		reference.Child("P06").SetValueAsync(false);
+		reference.Child("P07").SetValueAsync(false);
+		reference.Child("P08").SetValueAsync(false);
+		reference.Child("P09").SetValueAsync(false);
+		reference.Child("P10").SetValueAsync(false);
+
+		reference.Child("money").SetValueAsync("2000");
+
+	}
 }
