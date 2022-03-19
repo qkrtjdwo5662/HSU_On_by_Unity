ea81a524ada20b4102644f0f8bbfc4f2b02586ae
woohozang <71766148+woohozang@users.noreply.github.com>
2022-03-20 03:06:30 +0900

    No_Role_NPC 제작작업2
    에셋 스토어에서 무료 애니메이션 모션 부분 다운 (추위에 떨고있는 모션, 어리둥절하는 모션) 각자 추가했음. No_Role_NPC_Trigger.cs 수정 및 추가 작업

--- Assets/Scripts/Scene_Field/Quest/No_Role_NPC_Trigger.cs
+++ Assets/Scripts/Scene_Field/Quest/No_Role_NPC_Trigger.cs
@@ -17,13 +17,6 @@ public class No_Role_NPC_Trigger : MonoBehaviour
     
     private Button startBtn; //대화하기 버튼
 
-    //NPC1 Talk
-    public Text NPC1_Text;
-    public Button NPC1_Btn;
-    public string NPC1_Text_Next="";
-    public Button NPC1_X_Btn;
-    public int NPC1_Btn_Count = 0;
-    //NPC1 Talk 끝
     private GameObject CameraArm;
     private GameObject Me;
     private TPSCharacterController tps;
@@ -34,8 +27,116 @@ public class No_Role_NPC_Trigger : MonoBehaviour
     private void Start()
     {
         StartCoroutine(FindMe());
-        NPC1_Text.text = "여기가 바로 가상 한성대학교?";
     }
+
+    //NPC1 Talk
+    public Text NPC1_Text;
+    public Button NPC1_Btn;
+    public string NPC1_Text_First = "";
+    public string NPC1_Text_Next = "";
+    public Button NPC1_X_Btn;
+    public int NPC1_Btn_Count = 0;
+    //NPC1_Text_First = "여기가 바로 가상 한성대학교?";
+    //NPC1_Text.text = NPC1_Text_First.ToString();
+    public void NPC1_Talk()
+    {
+        
+        //NPC1_Btn.onClick.AddListener(NPC1_Btn_Count++);
+        if (NPC1_Btn_Count >= 1)
+        {
+            NPC1_Text_Next = "너도 신기하지 않니?";
+            NPC1_Text.text = NPC1_Text_Next.ToString();
+
+        }
+        else if (NPC1_Btn_Count == 0)
+        {
+            NPC1_Text.text = "여기가 바로 가상 한성대학교?";
+            NPC1_Btn_Count++;
+        }
+        NPC1_X_Btn.onClick.AddListener(() => {
+            NPC1_Btn_Count = 0;
+            NPC1_Text_Next = "여기가 바로 가상 한성대학교?";
+            NPC1_Text.text = NPC1_Text_Next.ToString();
+        });
+    }
+    //NPC1 Talk 끝
+
+    //NPC2 Talk
+    public Text NPC2_Text;
+    public Button NPC2_Btn;   
+    public Button NPC2_X_Btn;
+    public string NPC2_Text_Next = "";
+    public int NPC2_Btn_Count = 0;
+    
+    public void NPC2_Talk()
+    {
+        
+        //NPC2_Btn.onClick.AddListener(NPC2_Btn_Count++);
+        if (NPC2_Btn_Count >= 1)
+        {
+            NPC2_Text_Next = "아직까진 날이 춥구나.. (덜덜)";
+            NPC2_Text.text = NPC2_Text_Next.ToString();
+
+        }
+        else if(NPC2_Btn_Count == 0)
+        {
+            NPC2_Text.text = "멋 좀 내려고 한 껏 차려입고 왔는데";
+            NPC2_Btn_Count++;
+        }
+        NPC2_X_Btn.onClick.AddListener(() => {
+            NPC2_Btn_Count = 0;
+            NPC2_Text_Next = "멋 좀 내려고 한 껏 차려입고 왔는데";
+            NPC2_Text.text = NPC2_Text_Next.ToString();
+        });
+    }
+    //NPC2 Talk 끝
+
+    //NPC3 Talk
+    public Text NPC3_Text;
+    public Button NPC3_Btn;
+    public Button NPC3_X_Btn;
+    public string NPC3_Text_Next = "";
+    public int NPC3_Btn_Count = 0;
+
+    public void NPC3_Talk()
+    {
+        switch (NPC3_Btn_Count)
+        {
+            case 0:
+                NPC3_Text.text = "저기요 혹시 길 좀 물어봐도 될까요?";
+                NPC3_Btn_Count++;
+                break;
+            case 1:
+                NPC3_Text_Next = "상상파크가 어디에 있나요?";
+                NPC3_Text.text = NPC3_Text_Next.ToString();
+                NPC3_Btn_Count++;
+                break;
+            case 2:
+                NPC3_Text_Next = "아~ 연구관 1층이요? 감사합니다!";
+                NPC3_Text.text = NPC3_Text_Next.ToString();
+                NPC3_Btn_Count++;
+                break;
+        }
+        //NPC2_Btn.onClick.AddListener(NPC2_Btn_Count++);
+        /*if (NPC2_Btn_Count >= 1)
+        {
+            NPC2_Text_Next = "아직까진 날이 춥구나.. (덜덜)";
+            NPC2_Text.text = NPC2_Text_Next.ToString();
+
+        }
+        else if (NPC2_Btn_Count == 0)
+        {
+            NPC2_Text.text = "멋 좀 내려고 한 껏 차려입고 왔는데";
+            NPC2_Btn_Count++;
+        }*/
+        NPC3_X_Btn.onClick.AddListener(() => {
+            NPC3_Btn_Count = 0;
+            NPC3_Text_Next = "저기요 혹시 길 좀 물어봐도 될까요?";
+            NPC3_Text.text = NPC3_Text_Next.ToString();
+        });
+    }
+    //NPC3 Talk 끝
+
     /*public void Update()
     {
         NPC_Talk();
@@ -73,28 +174,7 @@ public class No_Role_NPC_Trigger : MonoBehaviour
             //FadeOut();
         }
     }
-    public void NPC_Talk()
-    {        
-        ++NPC1_Btn_Count;
-        if (NPC1_Btn_Count >= 1)
-        {
-            NPC1_Text_Next = "너도 신기하지 않니?";
-            NPC1_Text.text = NPC1_Text_Next.ToString();
-
-        }
-        /*else if (NPC1_Btn_Count == 0)
-        {
-            //NPC1_Text.text = "여기가 바로 가상 한성대학교?";
-
-            
-        }*/        
-        NPC1_X_Btn.onClick.AddListener(() => {
-            NPC1_Btn_Count = 0;
-            NPC1_Text_Next = "여기가 바로 가상 한성대학교?"; 
-            NPC1_Text.text = NPC1_Text_Next.ToString();
-        });
-        
-    }
+   
     private void CameraWork()
     {
 
