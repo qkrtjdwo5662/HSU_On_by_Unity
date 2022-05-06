ba2be463af5a53539760750cd33cdeff5f9c2993
yoongyu-hwang <dotheji11@naver.com>
2022-05-07 01:47:06 +0900

    길찾기 제작중-1

---
+++ Assets/Scripts/Scene_Field/FindWayManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindWayManager : MonoBehaviour
+{
+    public GameObject bugi;
+    public SangSangBugiNavigator navi;
+
+    public void GotoDestination(float x, float y, float z) 
+    {
+        bugi.SetActive(true);
+        navi.destiantion = new Vector3(x, y, z);
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Scene_Field/SangSangBugiNavigator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SangSangBugiNavigator : MonoBehaviour
+{
+	// 추적할 대상 Object
+	public Vector3 destiantion;
+	// 추적할 Object
+	private Transform tr;
+	// 추적 Object에 적용된 NavMeshAgent 컴포넌트
+	private NavMeshAgent nvAgent;
+	// Use this for initialization
+
+	public FindWayManager fm;
+
+	public Animator ani;
+	void Start()
+	{
+		nvAgent = GetComponent<NavMeshAgent>();
+		tr = GetComponent<Transform>();
+		ani.SetBool("isMove",true);
+		//추적 Object에 적용된 NavMeshAgent 컴포넌트에 추적대상 설정         
+		nvAgent.destination = destiantion;
+	}
+	// Update is called once per frame     
+	void Update()
+	{
+		nvAgent.destination = destiantion;
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        
+    }
+
+}
