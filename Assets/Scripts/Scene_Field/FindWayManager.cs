6e2b67fd311b6892f0507e062765100b02f34062
yoongyu-hwang <dotheji11@naver.com>
2022-05-12 03:41:26 +0900

    .

--- Assets/Scripts/Scene_Field/FindWayManager.cs
+++ Assets/Scripts/Scene_Field/FindWayManager.cs
@@ -7,23 +7,93 @@ public class FindWayManager : MonoBehaviour
 {
     public GameObject bugi;
     public SangSangBugiNavigator navi;
-    public Vector3 destination;
-    public Button btn;
     
+    public Transform Entry;
+    public Transform SangSang;
+    public Transform YeonGu;
+    public Transform NakSan;
+    public Transform ChangUi;
+    public Transform WooChon;
+    public Transform JanDi;
+    public Transform JinRi;
+    public Transform TamGu;
+    public Transform GongHakA;
+    public Transform GongHakB;
+    public Transform SangBil;
+    public Transform JiSun;
+    public Transform HakSong;
+    public Transform MiRae;
+    
+    public Button EntryBtn;
+    public Button SangSangBtn;
+    public Button YeonGuBtn;
+    public Button NakSanBtn;
+    public Button ChangUiBtn;
+    public Button WooChonBtn;
+    public Button JanDiBtn;
+    public Button JinRiBtn;
+    public Button TamGuBtn;
+    public Button GongHakABtn;
+    public Button GongHakBBtn;
+    public Button SangBilBtn;
+    public Button JiSunBtn;
+    public Button HakSongBtn;
+    public Button MiRaeBtn;
 
 
-
-    public void GotoDestination(float x, float y, float z) 
+    public void GotoDestination(Transform t) 
     {
         bugi.SetActive(true);
-        bugi.transform.position = GameObject.Find("Me").GetComponent<Transform>().position;
-        navi.destiantion = new Vector3(x, y, z);
-        navi.StartGuide();
+        bugi.transform.position = GameObject.Find("Me").GetComponent<Transform>().position + new Vector3(0, 2, 0);
+        navi.StartGuide(t);
 
     }
     void Start()
     {
-        
+        EntryBtn.onClick.AddListener(()=> {
+            GotoDestination(Entry.transform);
+        });
+        SangSangBtn.onClick.AddListener(() => {
+            GotoDestination(SangSang.transform);
+        });        
+        YeonGuBtn.onClick.AddListener(() => {
+            GotoDestination(YeonGu.transform);
+        });
+        NakSanBtn.onClick.AddListener(() => {
+            GotoDestination(NakSan.transform);
+        });
+        ChangUiBtn.onClick.AddListener(() => {
+            GotoDestination(ChangUi.transform);
+        });
+/*        WooChonBtn.onClick.AddListener(() => {
+            GotoDestination(WooChon.transform);
+        });*/
+        JanDiBtn.onClick.AddListener(() => {
+            GotoDestination(JanDi.transform);
+        });
+        JinRiBtn.onClick.AddListener(() => {
+            GotoDestination(JinRi.transform);
+        });
+        TamGuBtn.onClick.AddListener(() => {
+            GotoDestination(TamGu.transform);
+        });
+        GongHakABtn.onClick.AddListener(() => {
+            GotoDestination(GongHakA.transform);
+        });
+        GongHakBBtn.onClick.AddListener(() => {
+            GotoDestination(GongHakB.transform);
+        });
+        JiSunBtn.onClick.AddListener(() => {
+            GotoDestination(JiSun.transform);
+        });
+        HakSongBtn.onClick.AddListener(() => {
+            GotoDestination(HakSong.transform);
+        });
+        MiRaeBtn.onClick.AddListener(() => {
+            GotoDestination(MiRae.transform);
+        });
+
+
     }
 
     // Update is called once per frame
--- Assets/Scripts/Scene_Field/SangSangBugiNavigator.cs
+++ Assets/Scripts/Scene_Field/SangSangBugiNavigator.cs
@@ -6,73 +6,81 @@ using UnityEngine.AI;
 public class SangSangBugiNavigator : MonoBehaviour
 {
 	// 추적할 대상 Object
-	public Vector3 destiantion;
 	// 추적할 Object
 	private Transform tr;
 	// 추적 Object에 적용된 NavMeshAgent 컴포넌트
-	private NavMeshAgent nvAgent;
+	public NavMeshAgent nvAgent;
+
+	private Vector3 destination;
 	// Use this for initialization
 	public bool moveSwitch = false;
 	public bool isArrive = false;
-	public GameObject Me;
 
-
-	public FindWayManager fm;
-
-
-
-	public Animator ani;
+	[SerializeField]
+	private GameObject Me;
+	[SerializeField]
+	private Animator ani;
 	void Start()
 	{
 		Me = GameObject.Find("Me");
-		nvAgent = GetComponent<NavMeshAgent>();
 		tr = GetComponent<Transform>();
-		ani.SetBool("isMove",false);
+		//ani.SetBool("isMove", false);
 		//추적 Object에 적용된 NavMeshAgent 컴포넌트에 추적대상 설정         
-		nvAgent.destination = destiantion;
 	}
 
 
-	public void StartGuide() {
-		ani.SetBool("isMove", true);
+	public void StartGuide(Transform t)
+	{
+
+		Debug.Log(t.position);
+		//ani.SetBool("isMove", true);
 		moveSwitch = true;
 		isArrive = false;
+		destination = t.position;
+		nvAgent.destination = destination;
+
 	}
 
 
 	// Update is called once per frame     
 	void Update()
 	{
-		if (moveSwitch)
-		{
-			nvAgent.destination = destiantion;
-		}
-	//	if (Vector3.Distance(this.transform.position, Me.trnasform.position >= 2.5f)) {
-		//	moveSwitch = false;
-			//this.transform.LookAt(Me);
-		//}
-	}
-    private void OnTriggerEnter(Collider other)
-    {
-		if (other.tag == "Player")
-		{
-			if(isArrive) {
-				//음성 출
-			}
-			else if (!isArrive)
-			{
-				moveSwitch = true;
-			}
-		}
-		else if (other.tag == "Flag") {
-			moveSwitch = false;
-			isArrive = true;
-			ani.SetBool("isMove",false);
-		}
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        
-    }
+        if (Vector3.Distance(this.transform.position, Me.transform.position) >= 7.5f)
+        {
+            moveSwitch = false;
+            nvAgent.isStopped = true;
+            this.transform.LookAt(Me.transform);
+			//ani.SetBool("isMove", false);
 
+		}
+	}
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.tag == "Player")
+        {
+            if (isArrive)
+            {
+                //음성 출
+            }
+            else if (!isArrive)
+            {
+                Debug.Log("@@@@@@");
+                nvAgent.isStopped = false;
+				//ani.SetBool("isMove", true);
+
+			}
+		}
+        else if (other.tag == "Flag")
+        {
+            nvAgent.isStopped = true;
+            isArrive = true;
+            //ani.SetBool("isMove", false);
+        }
+    }
+	private void OnTriggerExit(Collider other)
+	{
+
+
+
+	}
 }
