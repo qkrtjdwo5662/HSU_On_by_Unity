80a712c8f0da42172ef2034bdb82f6828793c527
yoongyu-hwang <dotheji11@naver.com>
2022-05-14 03:15:06 +0900

    도착하거나 멀어지면 대화창 츨력

--- Assets/Scripts/Scene_Field/SangSangBugiNavigator.cs
+++ Assets/Scripts/Scene_Field/SangSangBugiNavigator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -16,6 +17,11 @@ public class SangSangBugiNavigator : MonoBehaviour
 	public bool moveSwitch = false;
 	public bool isArrive = false;
 
+	public GameObject SpeechDialog;
+	public TextMeshPro text;
+	private GameObject CameraArm;
+
+
 	[SerializeField]
 	private GameObject Me;
 	[SerializeField]
@@ -24,7 +30,9 @@ public class SangSangBugiNavigator : MonoBehaviour
 	{
 		Me = GameObject.Find("Me");
 		tr = GetComponent<Transform>();
-		//ani.SetBool("isMove", false);
+		ani.SetBool("isMove", false);
+		CameraArm = GameObject.Find("Camera Arm");
+
 		//추적 Object에 적용된 NavMeshAgent 컴포넌트에 추적대상 설정         
 	}
 
@@ -33,7 +41,7 @@ public class SangSangBugiNavigator : MonoBehaviour
 	{
 
 		Debug.Log(t.position);
-		//ani.SetBool("isMove", true);
+		ani.SetBool("isMove", true);
 		moveSwitch = true;
 		isArrive = false;
 		destination = t.position;
@@ -45,38 +53,47 @@ public class SangSangBugiNavigator : MonoBehaviour
 	// Update is called once per frame     
 	void Update()
 	{
-        if (Vector3.Distance(this.transform.position, Me.transform.position) >= 7.5f)
+		if (SpeechDialog.activeInHierarchy)
+		{
+			SpeechDialog.transform.rotation = CameraArm.transform.rotation;
+		}
+		if (Vector3.Distance(this.transform.position, Me.transform.position) >= 7.5f)
         {
             moveSwitch = false;
             nvAgent.isStopped = true;
             this.transform.LookAt(Me.transform);
-			//ani.SetBool("isMove", false);
-
+			ani.SetBool("isMove", false);
+			SpeechDialog.SetActive(true);
+			text.text = "내가 너무 빠르구나? 기다리고 있을게!";
+		}
+		if (isArrive) {
+			this.transform.LookAt(Me.transform);
+			SpeechDialog.SetActive(true);
+			text.text = "도착했어! 이곳이 목적지야!";
 		}
 	}
 	private void OnTriggerEnter(Collider other)
 	{
         if (other.tag == "Player")
         {
-            if (isArrive)
+            if (!isArrive)
             {
-                //음성 출
-            }
-            else if (!isArrive)
-            {
-                Debug.Log("@@@@@@");
                 nvAgent.isStopped = false;
-				//ani.SetBool("isMove", true);
+				ani.SetBool("isMove", true);
+				SpeechDialog.SetActive(false);
 
 			}
 		}
-        else if (other.tag == "Flag")
+        if (other.tag == "Destination")
         {
+			Debug.Log("@@@@@@@@@");
             nvAgent.isStopped = true;
             isArrive = true;
-            //ani.SetBool("isMove", false);
-        }
-    }
+            ani.SetBool("isMove", false);
+			this.transform.LookAt(Me.transform);
+
+		}
+	}
 	private void OnTriggerExit(Collider other)
 	{
 
