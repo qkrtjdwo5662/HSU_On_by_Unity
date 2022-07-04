4fc2f17c5b285fa398a968e8ae0a4603d2809460
qkrtjdwo5662 <71877347+qkrtjdwo5662@users.noreply.github.com>
2022-07-05 00:28:59 +0900

    Dialog Script1

---
+++ Assets/Scripts/Scene_Field/dialog/Dialog.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialog : MonoBehaviour
+{
+    Dictionary<string, string[]> talkData;
+
+    void Awake()
+    {
+        talkData = new Dictionary<string, string[]>();
+        GenerateData();
+    }
+
+    void GenerateData()
+    {
+        talkData.Add("TestNPC0", new string[] {
+            "안녕 어서 와 ! HSU_ON에 온 걸 환영해 !!",
+            "지금부터 내 소개를 해줄게 !나는 퀘스트 가이드 상상부기야 ! 만나서 반가워 ~!",
+            "자! 지금부터 나와 같이 몇 가지 퀘스트를 진행할거야 ! 학교에 곳곳에 있는 나를 찾아와서 미션을 수행하면 돼 !",
+            "퀘스트의 미션을 완료하면 내가 스탬프를 하나씩 찍어주도록 하지 ! 모든 미션을 완료하면 상품이 기다리고 있으니 기대해도 좋아 ~",
+            "바로 시작하자 ! 준비됐지 ?"
+        });
+        talkData.Add("TestNPC1", new string[] {
+            "어 !! 여기야 여기 !!",
+            "잘 찾아왔구나 ~여기가 미래관이라는 건물이야 !",
+            "미래관에는 학교 도서관과 독서실 등 자유롭게 공부할 수 있는 공간이 구비되어 있어 !",
+            "다음에 꼭 나랑 같이 공부하기야 ~약속 !!",
+            "자 ! 첫 번째 미션은 학교를 돌아다녀서 암호를 찾아볼 거야 !",
+            "내가 근처에 암호를 숨겨놨어 !숨겨진 암호를 찾고 나를 찾아와서 암호를 맞추면 미션 클리어야 ~ ",
+            "미션 바로 시작해 볼까 ??"
+        });
+        talkData.Add("Yes", new string[]
+        {
+            "굳",
+            "멋지군"
+        });
+        talkData.Add("No", new string[]
+        {
+            "빠이빠이빠이빠이빠이",
+            "수고해 학생 ~"
+        });
+    }
+
+    public string GetTalk(string name, int talkIndex)
+    {
+        return talkData[name][talkIndex];
+    }
+    public int getSize(string name)
+    {
+        return talkData[name].Length;
+    }
+}
---
+++ Assets/Scripts/Scene_Field/dialog/Trigger.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+
+public class Trigger : MonoBehaviour
+{
+    public Dialog dialog; //dialog script
+    public int size = 0; // dialog talkdata length
+
+    public GameObject Talk0; //Button object
+    public Button StartButton; //button from Talk0
+
+    public GameObject Talk1; // Text, Button Object
+    public Button NextButton; // button from Talk1(next)
+    public Text text; // text from Talk1
+    public int ButtonCount = 0; // Nextbutton click count
+    public Button XButton; // button from Talk1(exit)
+
+    public GameObject Talk2;
+    public Button YesButton; // button from Talk2(yes)
+    public Button NoButton; // button from Talk2(no)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        size = dialog.getSize(gameObject.name);
+        StartButton.onClick.AddListener(() => //lamda function
+        {
+
+            Talk0.SetActive(false);
+            Talk1.SetActive(true);
+            Debug.Log(gameObject.name);
+            StartCoroutine(TypingEffect());
+            //카메라 온
+
+        });
+        NextButton.onClick.AddListener(() => {
+            if (ButtonCount < size)
+            {
+                StartCoroutine(TypingEffect());
+                ButtonCount++;
+                if (ButtonCount == size - 1)
+                {
+                    NextButton.gameObject.SetActive(false); // Talk2와 NextButton이 겹쳐 보이는 문제
+                    Talk2.SetActive(true);
+                }
+            }
+            else
+            {
+                ButtonCount = 0;
+                Talk1.SetActive(false);
+                NextButton.gameObject.SetActive(true); // NextButton 꺼진거 다시키기
+                Talk2.SetActive(false);
+                //카메라 리턴
+            }
+        });
+        XButton.onClick.AddListener(() =>
+        {
+            ButtonCount = 0;
+            Talk0.SetActive(false);
+            Talk1.SetActive(false);
+            NextButton.gameObject.SetActive(true); // X버튼 눌렀을때 NextButton 다시켜기
+            Talk2.SetActive(false);
+            //카메라 리턴
+        });
+        YesButton.onClick.AddListener(() =>
+        {
+            //GetTalk(Yes, ButtonCount)가져오기
+            //Problem 1. GetTalk의 값을 여기서 지정해주는 것이 가능한가?
+            //Problem 2. NPC마다의 Yes버튼 눌렀을때 대화를 통일하는 것이 맞을까?
+            //Problem 3. NPC마다의 Yes버튼 눌렀을때 각 다른 이벤트 처리는 어떻게 할 것인가? (NPC0에서 Yes버튼 클릭시 Quest창 true)
+        });
+        NoButton.onClick.AddListener(() =>
+        {
+            //GetTalk(No, ButtonCount)가져오기
+            //Problem 1. GetTalk의 값을 여기서 지정해주는 것이 가능한가?
+        });
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && other.GetComponent<PhotonView>().IsMine)
+        {
+            Talk0.SetActive(true);
+
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        ButtonCount = 0;
+        Talk0.SetActive(false);
+        Talk1.SetActive(false);
+        //카메라 리턴
+    }
+    IEnumerator TypingEffect()
+    {
+        for (int i = 0; i < dialog.GetTalk(gameObject.name, ButtonCount).Length; i++)
+        {
+            text.text = dialog.GetTalk(gameObject.name, ButtonCount).Substring(0, i);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
+
+}
