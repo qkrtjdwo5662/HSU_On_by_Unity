8168b097dbc945c240dd188a751417f45ed43b45
qkrtjdwo5662 <71877347+qkrtjdwo5662@users.noreply.github.com>
2022-07-05 23:17:16 +0900

    Dialog Script 2 - yes/no button

--- Assets/Scripts/Scene_Field/dialog/Trigger.cs
+++ Assets/Scripts/Scene_Field/dialog/Trigger.cs
@@ -17,10 +17,12 @@ public class Trigger : MonoBehaviour
     public Text text; // text from Talk1
     public int ButtonCount = 0; // Nextbutton click count
     public Button XButton; // button from Talk1(exit)
+    public Button NextButton2; // button from nextbutton
 
     public GameObject Talk2;
     public Button YesButton; // button from Talk2(yes)
     public Button NoButton; // button from Talk2(no)
+
 
     // Start is called before the first frame update
     void Start()
@@ -44,6 +46,7 @@ public class Trigger : MonoBehaviour
                 if (ButtonCount == size - 1)
                 {
                     NextButton.gameObject.SetActive(false); // Talk2와 NextButton이 겹쳐 보이는 문제
+                    NextButton2.gameObject.SetActive(false);
                     Talk2.SetActive(true);
                 }
             }
@@ -67,8 +70,39 @@ public class Trigger : MonoBehaviour
         });
         YesButton.onClick.AddListener(() =>
         {
+            ButtonCount = 0;
+            Talk2.SetActive(false);
+            NextButton2.gameObject.SetActive(true);
+            Debug.Log(YesButton.name);
+            StopCoroutine(TypingEffect());
+            StopCoroutine(TypingEffect2());
+
+            StartCoroutine(TypingEffect1());
+            NextButton2.onClick.AddListener(() =>
+            {
+                if (ButtonCount < size)
+                {
+                    StartCoroutine(TypingEffect1());
+                    ButtonCount++;
+                    
+                }
+                else
+                {
+                    ButtonCount = 0;
+                    Talk1.SetActive(false);
+                    NextButton.gameObject.SetActive(true); // NextButton 꺼진거 다시키기
+                    NextButton2.gameObject.SetActive(false);
+                    Talk2.SetActive(false);
+                    //카메라 리턴
+                }
+            });
             //GetTalk(Yes, ButtonCount)가져오기
             //Problem 1. GetTalk의 값을 여기서 지정해주는 것이 가능한가?
+            //Solution 1. TypingEffect1()함수 추가 (YesButton 클릭시에 Yes_Button의 value값 가져옴)
+            //Solution 2. YesButton.onClick.AddListener 추가 : ButtonCount초기화, Yes/No Button 끄기, TypingEffect1() 코루틴 시작, nextbutton 복구
+            //Solution 3. NextButton2를 만들어줌
+
+
             //Problem 2. NPC마다의 Yes버튼 눌렀을때 대화를 통일하는 것이 맞을까?
             //Problem 3. NPC마다의 Yes버튼 눌렀을때 각 다른 이벤트 처리는 어떻게 할 것인가? (NPC0에서 Yes버튼 클릭시 Quest창 true)
         });
@@ -76,6 +110,8 @@ public class Trigger : MonoBehaviour
         {
             //GetTalk(No, ButtonCount)가져오기
             //Problem 1. GetTalk의 값을 여기서 지정해주는 것이 가능한가?
+            //Solution TypingEffect1()함수 추가
+            StartCoroutine(TypingEffect2());
         });
 
     }
@@ -103,5 +139,21 @@ public class Trigger : MonoBehaviour
             yield return new WaitForSeconds(0.02f);
         }
     }
+    IEnumerator TypingEffect1()
+    {
+        for (int i = 0; i < dialog.GetTalk(YesButton.name, ButtonCount).Length; i++)
+        {
+            text.text = dialog.GetTalk(YesButton.name, ButtonCount).Substring(0, i);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
 
+    IEnumerator TypingEffect2()
+    {
+        for (int i = 0; i < dialog.GetTalk(NoButton.name, ButtonCount).Length; i++)
+        {
+            text.text = dialog.GetTalk(NoButton.name, ButtonCount).Substring(0, i);
+            yield return new WaitForSeconds(0.02f);
+        }
+    }
 }
